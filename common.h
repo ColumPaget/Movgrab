353e9770d64082b18a15bce683b627ace2bfd81b
Colum Paget <colums.projects@gmail.com>
2017-07-20 00:29:22 +0100

    Version bump

--- common.h
+++ common.h
@@ -3,7 +3,7 @@
 #define MOVGRAB_COMMON
 
 //This is doable through autoconf, but I'm sick of fighting with it
-#define Version "3.1.1"
+#define Version "3.1.2"
 
 #include "libUseful-2.8/libUseful.h"
 #include <string.h>
